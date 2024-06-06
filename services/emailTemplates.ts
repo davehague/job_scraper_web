a59089f7453fa31c6de7d979b7da31581231df40
David Hague <david.hague@gmail.com>
2024-06-06 10:22:27 -0400

    Added the framework for sending emails from the app

---
+++ services/emailTemplates.ts
@@ -0,0 +1,54 @@
+export const addedToGroup = `
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+            background-color: #f4f4f4;
+            color: #333;
+        }
+        .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            max-width: 600px;
+            margin: 20px auto;
+        }
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            border: none;
+            text-decoration: none;
+            border-radius: 4px;
+            background-color: #007bff;
+            color: white !important;
+            cursor: pointer;
+            margin: 20px 0px;
+        }
+        .footer {
+            margin-top: 20px;
+            font-size: 12px;
+            text-align: center;
+            color: #aaa;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Welcome to the Job App!</h2>
+        <p>To get started, simply click the button below.</p>
+        <span style="display: block; text-align: center;">
+            <a href="#" class="button">Login to the Jobs App</a>
+        </span>
+        <p>If you have any questions or need assistance, feel free to reply to this email and we'll get back to you in a jiffy.</p>
+        <div class="footer">
+            <p>Happy searching!</p>
+        </div>
+    </div>
+</body>
+</html>
+`;
